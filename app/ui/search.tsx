0af71757d0cf7e81ef64c0f974a5dfd5f33da1d5
morihara <morihara@ayanas.co>
2025-08-27 08:13:45 +0900

    u

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,6 +7,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
