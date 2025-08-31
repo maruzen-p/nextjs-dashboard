47753cbf305ef764f408f41e802579d9da302df7
morihara <morihara@ayanas.co>
2025-08-31 17:20:46 +0900

    u

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,10 +12,12 @@ const FormSchema = z.object({
   customerId: z.string(),
   amount: z.coerce.number(),
   status: z.enum(["pending", "paid"]),
-  data: z.string(),
+  date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, data: true });
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -29,6 +31,24 @@ export async function createInvoice(formData: FormData) {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  console.log(formData);
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    ;
   `;
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
