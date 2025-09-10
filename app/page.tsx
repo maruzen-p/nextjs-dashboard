30c0fbce75d1514a44e2deb315f512200d0327bb
morihara <morihara@ayanas.co>
2025-09-11 08:40:39 +0900

    三角形を追加

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className="relative w-0 h-0 border-l-[20px] border-r-[20px] border-b-[30px] border-l-transparent border-r-transparent border-b-black"></div>
           <div className={home.shape}></div>
+          <div className={home.shape}></div>
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
