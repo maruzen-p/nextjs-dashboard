e7b3848e40300e5669a617339c5e7f6f24c05e1b
morihara <morihara@ayanas.co>
2025-09-16 08:10:36 +0900

    u

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
-export default function Page() {
+import { fetchRevenue } from "@/app/lib/data";
+export default async function Page() {
+  const revenueData = await fetchRevenue();
   return <p>Invoices Page</p>;
 }
